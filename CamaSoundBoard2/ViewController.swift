e9743616dbf739da5eb73989a471f50696e2f84a
JosephLeoPoldo <eledar123@gmail.com>
2024-10-09 19:48:58 -0500

    Initial Commit

---
+++ CamaSoundBoard2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CamaSoundBoard2
+//
+//  Created by Bryan Joseph Cama Mamani on 9/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
